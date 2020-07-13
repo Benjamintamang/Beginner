9d9ab744ead1672f4e7dcff63a846e2da3a77315
Benjamin Tamang <bptamang29@gmail.com>
2020-07-13 18:24:40 +0545

    first commit

---
+++ inputExample.R
@@ -0,0 +1,5 @@
+my.name <- readline(prompt = "Enter name:")
+my.age <- readline(prompt = "Enter Age:")
+my.age <- as.integer(my.age)
+
+print(paste("Hi", my.name , "next year you will be", my.age+1, "years old"))
